7a47694a7430b062d460a5400bcd51c0fbdb8b5b
aswathy3530 <36253922+aswathy3530@users.noreply.github.com>
2018-04-11 21:27:09 +0530

    Add files via upload

---
+++ perex.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+#basic print 
+print "Greetings ! \n";
+ $v1=13;
+ $v2=20;
+print "sum of $v1 , $v2 : \n";
+ $v3= $v1 + $v2;
+print " = $v3 \n";
+
+#array
+@arr=(5,6,7,3,1);
+print @arr ;
+print"\n";
+%hash = ('Zai' => 16 , 'Ray' => 60, 'Thie' => 13 );
+print "\n";
+print $hash{'Thie'};
+print "\n";
